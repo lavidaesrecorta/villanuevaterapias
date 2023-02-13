6aee76cb4d0bfd8749e6f4255b7efa4b8683536d
Rodrigo Jiménez <jimenez.rodrigo.dev@gmail.com>
2023-02-12 23:43:48 -0300

    feat: added title and description meta-tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Andrea Villanueva | Terapias Alternativas - Alcanza la paz interior</title>
+        <meta name="description" content="Descubre el poder de la terapia alternativa - Experimenta una salud óptima, niveles de energía aumentados y equilibrio en tu día a día." />
+      </Head>
       <body>
         <Main />
         <NextScript />
